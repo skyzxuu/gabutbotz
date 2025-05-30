ea7a0a42ce89ccba2c6f6b4f11c4cfab5bc085e7
skyzxuu <122117174+skyzxuu@users.noreply.github.com>
2025-05-30 22:46:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 ownerId: 1234567890,
+global.msg = {
+  wait: "Tunggu sebentar...",
+  eror: "Terjadi kesalahan"
+}
 global.api = {
-  fast: "https://fastrestapis.fasturl.cloud"
+  fast: "https://fastrestapis.fasturl.cloud",
+  vrd: "https://api.vreden.my.id"
 }
